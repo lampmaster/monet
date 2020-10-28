d3e221e1d520e76a12f411d60f46e75c47281cc6
lamp <paperslink@gmail.com>
2020-10-28 23:01:58 +0300

    implemented auth page

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,146 @@
+import React, {Component} from "react";
+import classes from './Auth.module.scss'
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
+import {Logo} from "../Main/components/Logo/Logo";
+import {validate, validateForm} from "../../form/FormFramework";
+import {useHistory} from 'react-router-dom'
+import axios from "axios";
+
+
+export default class Auth extends Component {
+
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                placeholder: 'E-mail',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                placeholder: 'Password',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
+    loginHandler = async () => {
+        // await axios.get('https://monet-c1972.firebaseio.com/quiz.json').then(res => console.log(res))
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+
+        try {
+            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyADV_5Sar3NIHyNS5weDyxiQofUtc3nPOE', authData)
+            const history = useHistory()
+            history.push('/')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    registration = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+
+        try {
+            const res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyADV_5Sar3NIHyNS5weDyxiQofUtc3nPOE', authData)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    submitHandler = (event) => {
+        event.preventDefault()
+    };
+
+    onChangeHandler = (event, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+        control.value = event.target.value
+        control.touched = true
+        control.valid = validate(control.value, control.validation)
+
+        formControls[controlName] = control
+        const isFormValid = validateForm(formControls)
+
+        this.setState({
+            formControls,
+            isFormValid
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    value={control.value}
+                    type={control.type}
+                    placeholder={control.placeholder}
+                    valid={control.valid}
+                    touched={control.touched}
+                    shouldValidate={!!control.validation}
+                    onChange={(event) => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
+
+    render() {
+
+
+        return (
+            <div className={classes.Auth}>
+                <div className={classes.Container}>
+                    <Logo/>
+                    <form
+                        onSubmit={this.submitHandler}
+                        className={classes.Authform}
+                    >
+                        <div className={classes.col}>
+                            {this.renderInputs()}
+                        </div>
+
+                        <div className={classes.col}>
+                            <Button
+                                type={'prime'}
+                                onClick={this.loginHandler}
+                                disabled={!this.state.isFormValid}
+                            >
+                                Log in
+                            </Button>
+
+                            <Button
+                                onClick={this.registration}
+                                disabled={!this.state.isFormValid}
+                            >
+                                Sigh up
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
